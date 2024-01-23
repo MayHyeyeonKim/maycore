f619a703342087b7462aa83d9078a42491b3cb74
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-23 15:08:08 -0800

    Studying the Core Principles of Spring with Ins YH Kim

---
+++ src/main/java/mayco/maycore/MaycoreApplication.java
@@ -0,0 +1,13 @@
+package mayco.maycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaycoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaycoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mayco/maycore/MaycoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mayco.maycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaycoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
