3fb332889388cfeb7a07e28c44311cf9a48f7835
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-24 03:57:22 -0800

    Main function execution & JUnit test code execution

---
+++ src/main/java/mayco/maycore/OrderApp.java
@@ -0,0 +1,26 @@
+package mayco.maycore;
+
+import mayco.maycore.member.Grade;
+import mayco.maycore.member.Member;
+import mayco.maycore.member.MemberService;
+import mayco.maycore.member.MemberServiceImpl;
+import mayco.maycore.order.Order;
+import mayco.maycore.order.OrderService;
+import mayco.maycore.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "member!", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("orderCal = " + order.calculatePrice());
+
+    }
+}
