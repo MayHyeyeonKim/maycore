cabab5fe81029a44d4cc9a6e5b645fafbddb91a0
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-25 02:49:32 -0800

    Processing of addressing violations of DIP & OCP in the OrderServiceImpl

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 
 ![Order&DiscountService](https://github.com/MayHyeyeonKim/maycore/blob/main/Order&DiscountService.png)
 
+# Spring-based Order & Discount Policy
+The client code OrderServiceImpl violates the Dependency Inversion Principle (DIP) by depending on not only the DiscountPolicy interface but also the concrete class. To adhere to DIP, the dependency should be modified to rely solely on the interface.
+![DIP&OCP](https://github.com/MayHyeyeonKim/maycore/blob/main/DIPOCP.png)
+However, if there is no implementation available, how can the code be executed? In practice, attempting to run the code would result in a NullPointerException (NPE).
+![NullPointerException](https://github.com/MayHyeyeonKim/maycore/blob/main/NullPointerException.png)
+
+
--- src/main/java/mayco/maycore/order/OrderServiceImpl.java
+++ src/main/java/mayco/maycore/order/OrderServiceImpl.java
@@ -1,7 +1,8 @@
 package mayco.maycore.order;
 
 import mayco.maycore.discount.DiscountPolicy;
-import mayco.maycore.discount.FixDiscountPolicy;
+//import mayco.maycore.discount.FixDiscountPolicy;
+//import mayco.maycore.discount.RateDiscountPolicy;
 import mayco.maycore.member.Member;
 import mayco.maycore.member.MemberRepository;
 import mayco.maycore.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import mayco.maycore.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy;
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
