98b9ba21fcd7816c9d54b0394de3eb4c6961f2fd
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-26 02:45:40 -0800

    Spring Container & Bean

--- src/main/java/mayco/maycore/OrderApp.java
+++ src/main/java/mayco/maycore/OrderApp.java
@@ -7,12 +7,18 @@ import mayco.maycore.member.MemberServiceImpl;
 import mayco.maycore.order.Order;
 import mayco.maycore.order.OrderService;
 import mayco.maycore.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member!", Grade.VIP);
