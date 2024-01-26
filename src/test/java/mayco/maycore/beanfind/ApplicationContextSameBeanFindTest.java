92a4350dc2f3bf8985bc8545acc365e6ff5ef910
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-26 06:12:38 -0800

    JUnit Dealing with duplicate bean types & retrieving all beans by type

---
+++ src/test/java/mayco/maycore/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,60 @@
+package mayco.maycore.beanfind;
+
+import mayco.maycore.AppConfig;
+import mayco.maycore.discount.DiscountPolicy;
+import mayco.maycore.member.MemberRepository;
+import mayco.maycore.member.MemoryMemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("It multiple beans of the same type during type retrieval, dupl err occurs")
+    void findBeanByTypeDuplicate(){
+        MemberRepository bean = ac.getBean(MemberRepository.class);
+        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("When using type-based bean retrieval, if multiple beans of the same type, can specify beanname")
+    void findBeanByName(){
+        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+
+    }
+
+    @Test
+    @DisplayName("Retrieve all beans of a specific type")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + "value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+
+    }
+
+    @Configuration
+    static class SameBeanConfig{
+        @Bean
+        public MemberRepository memberRepository1(){
+            return new MemoryMemberRepository();
+        }
+        @Bean
+        public MemberRepository memberRepository2(){
+            return new MemoryMemberRepository();
+        }
+    }
+}
