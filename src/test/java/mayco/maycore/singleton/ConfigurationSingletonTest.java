381ec4004c4b415a71e98a382bf94f947ceb9f02
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-27 01:51:08 -0800

    Configuration & Singleton

---
+++ src/test/java/mayco/maycore/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,32 @@
+package mayco.maycore.singleton;
+
+import mayco.maycore.AppConfig;
+import mayco.maycore.member.MemberRepository;
+import mayco.maycore.member.MemberService;
+import mayco.maycore.member.MemberServiceImpl;
+import mayco.maycore.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest(){
+       ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
