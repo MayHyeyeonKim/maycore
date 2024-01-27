386c78b4641e0f5011b0833c2c83fee000eb1603
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-27 02:00:33 -0800

    without @Configuration

--- src/main/java/mayco/maycore/AppConfig.java
+++ src/main/java/mayco/maycore/AppConfig.java
@@ -11,7 +11,7 @@ import mayco.maycore.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class AppConfig {
     @Bean
     public MemberService memberService(){
